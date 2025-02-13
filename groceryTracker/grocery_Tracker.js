ede814acfc26ebf31948b4d911db6d8880f66577
gisellegalan <ggalan2@uic.edu>
2025-02-12 21:05:55 -0500

    lab: practice task - fixed error

--- groceryTracker/grocery_Tracker.js
+++ groceryTracker/grocery_Tracker.js
@@ -3,9 +3,9 @@ let grocery2;
 let grocery3;
 
 function groceryTracker() {
-    grocery1 = parseFloat(documet.getElementById('grocery1').value);
-    grocery2 = parseFloat(documet.getElementById('grocery2').value);
-    grocery3 = parseFloat(documet.getElementById('grocery3').value);
+    grocery1 = parseFloat(document.getElementById('grocery1').value);
+    grocery2 = parseFloat(document.getElementById('grocery2').value);
+    grocery3 = parseFloat(document.getElementById('grocery3').value);
 
     let totalAmount = grocery1 + grocery2 + grocery3;
 
