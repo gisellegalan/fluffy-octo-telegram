ec81489f6ed225faaf6ee6d782c3ff6eda7ff3ea
gisellegalan <ggalan2@uic.edu>
2025-03-03 22:24:31 -0600

    commented-out codereflects original

--- bookSystem/book_system.js
+++ bookSystem/book_system.js
@@ -27,8 +27,7 @@ function showbooks() {
         //<p><strong>Book Name: </strong>${book.name}</p>
         //<p><strong>Author Name:</strong> ${book.authorName}</p>
         //<p><strong>Book Description:</strong> ${book.bookDescription}</p>
-        //<p><strong>No. of Pages:</strong> ${book.pagesNumber} page(s)</p>
-        //<button onclick="deleteBook(${index})">Delete Book</button>`
+        //<p><strong>No. of Pages:</strong> ${book.pagesNumber} page(s)</p>`
     //);
 
     // practice task: delete button/function
